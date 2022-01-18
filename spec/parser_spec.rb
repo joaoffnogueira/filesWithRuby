d0077ed7fc0948c991b7dacf4240c14f0fee64e9
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 11:55:58 -0300

    add tests for file opening

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,12 +1,24 @@
 require_relative "../parser"
 
 RSpec.describe Parser do
-    context "log is games.log" do
-      it "and parser prints its first line" do
-        file = File.open("./games.log", 'r')
-        parser = Parser.new(file)
-        parser.print_first_line
-        expect "0:00 ------------------------------------------------------------"
-      end
+  context "if file not found" do
+    it "raises exception" do
+      path = "./not_found.log"
+      expect "File not found"
     end
+  end
+  context "file exists" do
+    it "so open the file" do
+      file = File.open("./games.log", 'r')
+      expect File.exists?(file)
+    end
+  end
+  context "log is games.log" do
+    it "and parser prints its first line" do
+      file = File.open("./games.log", 'r')
+      parser = Parser.new(file)
+      parser.print_first_line
+      expect "0:00 ------------------------------------------------------------"
+    end
+  end
 end
