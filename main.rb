cf2492f57a823ecf8f7da86004f2add2edc42a3c
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 08:20:48 -0300

    create main function

---
+++ main.rb
@@ -0,0 +1,11 @@
+def main
+    puts "Indicate the path to your log:"
+    path = gets.chomp
+    log = Log.new(File.new(path))
+    parser = Parser.new(log)
+    parser.print_first_line
+end
+
+if __FILE__ == $0
+    main
+end
