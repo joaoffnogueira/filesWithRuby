3bff212b1428d51cfe695a9aeb1bec14fb5f9548
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 10:21:45 -0300

    test print_first_line

---
+++ spec/parser_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../parser"
+
+RSpec.describe Parser do
+    context "log is games.log" do
+      it "prints first line" do
+        file = File.open("./games.log", 'r')
+        parser = Parser.new(file)
+        parser.print_first_line
+        expect "0:00 ------------------------------------------------------------"
+      end
+    end
+end
