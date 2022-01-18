d77affb8015824daa57f7e1b601ffcd1bb5332ba
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 14:18:40 -0300

    fix each test to each function

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,18 +1,25 @@
 require_relative "../parser"
 
-RSpec.describe Parser do
+RSpec.describe "#initialize" do
+  
   context "if file not found" do
     it "raises exception" do
       path = "./not_found.log"
       expect "File not found"
     end
   end
+  
   context "file exists" do
     it "so open the file" do
       file = File.open("./games.log", 'r')
       expect File.exists?(file)
     end
   end
+  
+end
+
+RSpec.describe "#print_first_line" do
+
   context "log is games.log" do
     it "and parser prints its first line" do
       file = File.open("./games.log", 'r')
@@ -21,4 +28,18 @@ RSpec.describe Parser do
       expect "0:00 ------------------------------------------------------------"
     end
   end
+
+end
+
+RSpec.describe "#close" do
+
+  context "parser is done" do
+    it "closes de file" do
+      @file = File.open("./games.log", 'r')
+      @file.close
+      @file
+      expect nil
+    end
+  end
+ 
 end
