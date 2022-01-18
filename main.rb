4a4c45adb905f75503bc75760d7fed5dd46a6799
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 10:48:03 -0300

    deal with exception File not found

--- main.rb
+++ main.rb
@@ -3,8 +3,11 @@ require_relative 'parser'
 def main
     puts "Indicate the path to your log:"
     path = gets.chomp
-    file = File.open(path, 'r')
-    raise "File not found" unless File.exists?(file)
+    begin
+        file = File.open(path, 'r')
+    rescue
+        raise "File not found"
+    end
     parser = Parser.new(file)
     parser.print_first_line
     file.close
