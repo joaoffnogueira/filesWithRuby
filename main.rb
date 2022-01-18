739bdbc6dbc29bb3c1bc006bc14b51200dc5897a
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 11:54:17 -0300

    remove accountabilities from main

--- main.rb
+++ main.rb
@@ -3,14 +3,9 @@ require_relative 'parser'
 def main
     puts "Indicate the path to your log:"
     path = gets.chomp
-    begin
-        file = File.open(path, 'r')
-    rescue
-        raise "File not found"
-    end
-    parser = Parser.new(file)
+    parser = Parser.new(path)
     parser.print_first_line
-    file.close
+    parser.close
 end
 
 if __FILE__ == $0
