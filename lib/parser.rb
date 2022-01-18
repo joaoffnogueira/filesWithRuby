7dbbe8f156d73ae3c5c156554b2931205a9b4969
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 19:35:11 -0300

    fix indentation

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,20 +1,20 @@
 class Parser
 
-    def initialize(path)
-        @path = path
-        begin
-            @file = File.open(path, 'r')
-        rescue
-            raise "File not found"
-        end
+  def initialize(path)
+    @path = path
+    begin
+        @file = File.open(path, 'r')
+    rescue
+        raise "File not found"
     end
+  end
 
-    def print_first_line
-        puts @file.readline
-    end
-    
-    def close
-        @file.close
-    end
+  def print_first_line
+    puts @file.readline
+  end
+  
+  def close
+    @file.close
+  end
     
 end
