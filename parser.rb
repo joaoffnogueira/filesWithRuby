80e0c627c32e88a989352820b7776382610f26e7
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-18 13:59:31 -0300

    increase readability on parser.rb

--- parser.rb
+++ parser.rb
@@ -1,4 +1,5 @@
 class Parser
+
     def initialize(path)
         @path = path
         begin
@@ -7,10 +8,13 @@ class Parser
             raise "File not found"
         end
     end
+
     def print_first_line
         puts @file.readline
     end
+    
     def close
         @file.close
     end
+    
 end
