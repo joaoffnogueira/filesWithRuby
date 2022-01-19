f4632a1b591fe23c1d79e71d002aef3752497e55
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-19 10:55:34 -0300

    fix test to_json - dependency of parser removed

--- spec/to_json_spec.rb
+++ spec/to_json_spec.rb
@@ -1,15 +1,14 @@
-require_relative "../lib/parser"
 require_relative "../lib/to_json"
 
 RSpec.describe "#to_json" do
 
-  context "log is test.log" do
-    it "and ToJSON puts a json object" do
-      file = File.open("./spec/test.log", 'r')
-      parser = Parser.new(file)
-      lines = parser.count_lines
-      full_path = File.expand_path(file)
+  context "providing full_path and lines" do
+    it "ToJSON puts a json object" do
+
+      full_path = "/home/jffn/filesWithRuby/spec/test.log"
+      lines = 10
       print = ToJSON.new(full_path, lines)
+
       expect(print.to_json).to eq("{\n  \"#{full_path}\": {\n    \"lines\": 10\n  }\n}")
     end
   end
