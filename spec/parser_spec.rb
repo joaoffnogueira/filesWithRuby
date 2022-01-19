d3fc18c56cc56e751130c79636951c82f7bcc3a7
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-19 10:53:05 -0300

    fix lb

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe "#initialize" do
   
   context "if file not found" do
     it "raises exception" do
-      path = "./not_found.log"
-      expect { raise "File not found" }.to raise_error(RuntimeError, "File not found")
+
+      expect{Parser.new("./not_found.log")}.to raise_error(RuntimeError, 'File not found')
     end
   end
   
   context "file exists" do
     it "so open the file" do
+
       file = File.open("./spec/test.log", 'r')
+
       expect(File.exists?(file)).to be true
     end
   end
@@ -22,8 +24,10 @@ RSpec.describe "#print_first_line" do
 
   context "log is test.log" do
     it "and parser prints its first line" do
+
       file = File.open("./spec/test.log", 'r')
       parser = Parser.new(file)
+
       expect(parser.print_first_line).to eq("first line of test.log\r\n")
     end
   end
@@ -34,8 +38,10 @@ RSpec.describe "#count_lines" do
 
   context "log is test.log" do
     it "and parser counts its lines" do
+
       file = File.open("./spec/test.log", 'r')
       parser = Parser.new(file)
+      
       expect(parser.count_lines).to eq(10)
     end
   end
