0055130e0981a83d276ec5dcd8f41ec28e36deaf
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-20 16:57:32 -0300

    fix rubocop warnings

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'json'
 
 class Parser
-
   def initialize(path)
     @path = path
     begin
@@ -14,24 +15,22 @@ class Parser
   def print_first_line
     @file.readline
   end
-  
+
   def close
     @file.close
   end
-  
+
   def output
     full_path = File.expand_path(@path)
     json = {
       full_path => {
         'lines' => count_lines()
-      } 
+      }
     }
     JSON.pretty_generate(json)
   end
-  
-  private
+
   def count_lines
     @file.readlines.size
   end
-
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,64 +1,53 @@
-require_relative "../lib/parser"
+# frozen_string_literal: true
 
-RSpec.describe "#initialize" do
-  
-  context "if file not found" do
-    it "raises exception" do
+require_relative '../lib/parser'
 
-      expect{Parser.new("./not_found.log")}.to raise_error(RuntimeError, 'File not found')
+RSpec.describe Parser do
+  context 'if file not found' do
+    it 'raises exception' do
+      expect { Parser.new('./not_found.log') }.to raise_error(RuntimeError, 'File not found')
     end
   end
-  
-  context "file exists" do
-    it "so open the file" do
 
-      file = File.open("./spec/test.log", 'r')
+  context 'file exists' do
+    it 'so open the file' do
+      file = File.open('./spec/test.log', 'r')
 
       expect(File.exists?(file)).to be true
     end
   end
-  
 end
 
-RSpec.describe "#print_first_line" do
-
-  context "log is test.log" do
-    it "and parser prints its first line" do
-
-      file = File.open("./spec/test.log", 'r')
+RSpec.describe '#print_first_line' do
+  context 'log is test.log' do
+    it 'and parser prints its first line' do
+      file = File.open('./spec/test.log', 'r')
       parser = Parser.new(file)
 
-      expect(parser.print_first_line).to eq("first line of test.log\r\n")
+      expect(parser.print_first_line).to eq('first line of test.log\r\n')
     end
   end
-
 end
 
-RSpec.describe "#count_lines" do
-
-  context "log is test.log" do
-    it "and parser counts its lines" do
-
-      file = File.open("./spec/test.log", 'r')
+RSpec.describe '#count_lines' do
+  context 'log is test.log' do
+    it 'and parser counts its lines' do
+      file = File.open('./spec/test.log', 'r')
       parser = Parser.new(file)
-      
+
       expect(parser.count_lines).to eq(10)
     end
   end
-
 end
 
-RSpec.describe "#output" do
-
-  context "log is test.log" do
-    it "parser puts returns as json" do
-
-      file = File.open("./spec/test.log", 'r')
+RSpec.describe '#output' do
+  context 'log is test.log' do
+    it 'parser puts returns as json' do
+      file = File.open('./spec/test.log', 'r')
       parser = Parser.new(file)
       full_path = File.expand_path(file)
 
-      expect(parser.output).to eq("{\n  \"#{full_path}\": {\n    \"lines\": 10\n  }\n}")
+      expect(parser.output).to eq("{\n  \'#{full_path}\': {\n    \'lines\': 10\n  }\n}")
     end
   end
-
 end
